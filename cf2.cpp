ba862dc4ae125b39f7fb9ddd3994483f1d7f12f0
fucode <33052433+shukla284@users.noreply.github.com>
2018-08-30 23:42:16 +0530

    Done with errors and now can find interval related numbers

--- cf2.cpp
+++ cf2.cpp
@@ -5,39 +5,32 @@
 
 using namespace std;
 
-#define lch(n) n<<2+1
-#define rch(n) n<<2+2
+#define lch(n) 2*n+1
+#define rch(n) 2*n+2
 #define chl cout<endl;
 #define vc vector
 #define ll long long
 #define var auto
 
 void update(vc<ll> &st,ll n,ll lo,ll hi,ll pos,ll val){
-	cout<<"called"<<lo<<" "<<hi<<"  "<<n<<endl;
+	
 	if(pos<lo or pos>hi) return;
-	if(lo==hi){
-		cout<<"updated with value "<<val<<endl;
-		cout<<st[n];
-		st[n]+=val;
-		cout<<st[n];return;}
+    st[n]+=val;
+    if(lo!=hi){
 	var mid=(lo+hi)>>1;
-	cout<<"\n\t\t\t"<<lch(n)<<"  "<<rch(n)<<endl;
 	update(st,lch(n),lo,mid,pos,val);
 	update(st,rch(n),mid+1,hi,pos,val);
 	st[n]=st[lch(n)]+st[rch(n)];	
+	}
 	return;
 }
 int query(vc<ll> st,ll n,ll lo, ll hi,ll ql,ll qh){
-	cout<<"called query"<<endl;
 	if(qh<lo or ql>hi)
 	  return 0;
 	if(qh>=hi and ql<=lo)
 	  return st[n];
-	cout<<lo<<" "<<hi<<endl;
 	var mid=(lo+hi)>>1;
-	var pl=query(st,lch(n),lo,mid,ql,qh);
-	var ph=query(st,rch(n),mid+1,hi,ql,qh);
-	return pl+ph;
+	return query(st,lch(n),lo,mid,ql,qh)+query(st,rch(n),mid+1,hi,ql,qh);
 }
 ll computeSize(ll n){
 	var ht=ceil(log2(n));
@@ -60,12 +53,10 @@ int main(int argc,char** argv){
 	vc<ll> st(computeSize(n),0);
 	
 	ll lo=0,hi=n-1;
-	//for(int i=0;i<n;i++)
-	//{
-		update(st,0,lo,hi,3,1);
-	printTree(st);
+	for(int i=0;i<n;i++)
+	{
+		update(st,0,lo,hi,qa[i],1);
+	}
 	cout<<query(st,0,lo,hi,2,5);
-	//}
-	
 	return 0;
 }
